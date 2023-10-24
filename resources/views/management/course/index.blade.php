c319bd070bf3389a3a8c1c1296fbd81d3e652f45
rafliiar17 <boozxtr008@gmail.com>
2023-10-24 09:29:47 +0700

    add view status active

--- resources/views/management/course/index.blade.php
+++ resources/views/management/course/index.blade.php
@@ -26,6 +26,15 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-6 shadow-md bg-white">
                 <x-splade-table :for="$schcourses">
+                    <x-splade-cell SchCourseStatus as="$schcourses">
+                        @if ($schcourses->SchCourseStatus==0)
+                            Non-Active
+                        @elseif ($schcourses->SchCourseStatus==1)
+                            Active
+                        @else 
+                            Blocked
+                        @endif
+                    </x-splade-cell>
                     <x-splade-cell actions as="$schcourses">
                         <Link href="{{ route('managements.courses.edit', $schcourses->id) }}" class="px-4 py-2 bg-gray-200 text-gray-700 rounded-md font-semibold hover:bg-yellow-400 hover:shadow-md hover:shadow-slate-200 hover:text-white mr-3"> Edit </Link>
                         <Link confirm="Hapus Config" confirm-text="Apakah kamu yakin akan menghapusnya?" confirm-button="Ya, hapus" cancel-button="Batal" href="{{ route('managements.courses.destroy', $schcourses->id) }}" method="DELETE" class="px-4 py-2 bg-slate-700 text-white rounded-md hover:bg-red-500 hover:shadow-md hover:shadow-slate-300"> Hapus </Link>
--- resources/views/management/grade/index.blade.php
+++ resources/views/management/grade/index.blade.php
@@ -26,6 +26,15 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-6 shadow-md bg-white">
                 <x-splade-table :for="$schgrade">
+                    <x-splade-cell SchStatus as="$schgrade">
+                        @if ($schgrade->SchStatus==0)
+                            Non-Active
+                        @elseif ($schgrade->SchStatus==1)
+                            Active
+                        @else 
+                            Blocked
+                        @endif
+                    </x-splade-cell>
                     <x-splade-cell actions as="$schgrade">
                         <Link href="{{ route('managements.grades.edit', $schgrade->id) }}" class="px-4 py-2 bg-gray-200 text-gray-700 rounded-md font-semibold hover:bg-yellow-400 hover:shadow-md hover:shadow-slate-200 hover:text-white mr-3"> Edit </Link>
                         <Link confirm="Hapus Config" confirm-text="Apakah kamu yakin akan menghapusnya?" confirm-button="Ya, hapus" cancel-button="Batal" href="{{ route('managements.grades.destroy', $schgrade->id) }}" method="DELETE" class="px-4 py-2 bg-slate-700 text-white rounded-md hover:bg-red-500 hover:shadow-md hover:shadow-slate-300"> Hapus </Link>
